ba96fa10cb0b7fc38a32bd65549670f7689db253
Tomer Gill <coolran103@gmail.com>
2018-03-20 15:21:07 +0200

    Fixed output

--- boolean_conj_predictor.py
+++ boolean_conj_predictor.py
@@ -9,6 +9,7 @@ Date:        20/03/18
 import numpy as np
 from sys import argv
 
+
 LITERAL = 1.0  # if the i-th cell in h is LITERAL, then x_i is in the conjunction
 NEGATION = 0.0  # likewise, but with not(x_i)
 BOTH = -1.0  # both x_i and not(x_i) are in the conjunction
@@ -54,10 +55,12 @@ def consistency_algorithm(X, Y):
 def result_to_string(result):
     output = ""
     for i in xrange(result.shape[0]):
-        if result[i] == 1:
-            output += "x{}".format(i)
-        elif result[i] == 0:
-            output += "not(x{})".format(i)
+        if result[i] == LITERAL:
+            output += "x{}".format(i+1)
+        elif result[i] == NEGATION:
+            output += "not(x{})".format(i+1)
+        elif result[i] == BOTH:
+            output += "x{},not(x{})".format(i+1, i+1)
         else:
             continue
         output += ","
@@ -79,4 +82,4 @@ if __name__ == '__main__':
     out_file = "./output.txt"
     if len(argv) > 1:
         in_file = argv[1]
-    main(in_file, out_file)
+    print main(in_file, out_file)
